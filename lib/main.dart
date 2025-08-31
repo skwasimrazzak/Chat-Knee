193978e2ffd418e78e7f65e038d1283c6463d35a
Wasim Razzak <wrazzak24@gmail.com>
2025-08-31 23:10:04 +0530

    Main & Theme Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
+import 'package:chat_knee/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(ChatKnee());
+}
+
+class ChatKnee extends StatelessWidget {
+  const ChatKnee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomeScreen(),
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.black45)),
+      ),
+    );
+  }
 }
