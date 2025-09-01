759582b6e9327c83e4e29076a22d95c9ee523a22
Wasim Razzak <wrazzak24@gmail.com>
2025-09-01 11:28:39 +0530

    Separating Constants

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kMessageContainerDecoration = BoxDecoration(
+  border: Border(top: BorderSide(color: Color(0xff26667F), width: 2.0)),
+);
+const kMessageTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  hintText: 'Message',
+  border: InputBorder.none,
+);
+const kTextButtonStyle = TextStyle(
+  color: Color(0xff26667F),
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+);
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,18 +1,5 @@
+import 'package:chat_knee/constants.dart';
 import 'package:flutter/material.dart';
-
-const kMessageContainerDecoration = BoxDecoration(
-  border: Border(top: BorderSide(color: Color(0xff26667F), width: 2.0)),
-);
-const kMessageTextFieldDecoration = InputDecoration(
-  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-  hintText: 'Message',
-  border: InputBorder.none,
-);
-const kTextButtonStyle = TextStyle(
-  color: Color(0xff26667F),
-  fontWeight: FontWeight.bold,
-  fontSize: 18.0,
-);
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
