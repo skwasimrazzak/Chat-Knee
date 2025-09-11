25a1c3ac37b7fc6f35324a937f3745160c51d272
Wasim Razzak <wrazzak24@gmail.com>
2025-09-11 19:17:20 +0530

    Firebase Setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,14 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
 }
 
+dependencies{
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+    implementation("com.google.firebase:firebase-analytics")
+}
 android {
     namespace = "com.example.chat_knee"
     compileSdk = flutter.compileSdkVersion
@@ -21,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.chat_knee"
+        applicationId = "com.skwasimrazzak.chat_knee"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
